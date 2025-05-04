26ee58df665455fbbb16d0f59bc7003b8acb1af4
Tejus Kandipilli <tejusking12447@gmail.com>
2025-05-04 19:28:35 +0530

    Finished project.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
-import db from '../db.js'
+
+import prisma from '../prismaClient.js'
 
 const router = express.Router()
 
 // Register a new user /auth/register
-router.post('/register', (req, res)=>{
+router.post('/register',async (req, res)=>{
   const {username, password} = req.body
   
   // encrypt password
@@ -14,16 +15,23 @@ router.post('/register', (req, res)=>{
 
   // save the new user and  
   try{
-    const insertUser = db.prepare(`INSERT INTO users (username,password) VALUES (?, ?)`)
-    const result = insertUser.run(username, hashedPassword)
+   const user = await prisma.user.create({
+    data:{
+      username,
+      password: hashedPassword
+    }
+   })
 
     // Adding their first todo for them
     const defaultTodo = 'Hello :) Add your first todo!'
-    const insertTodo = db.prepare('INSERT INTO todos (user_id, task) VALUES (?, ?)')
-    insertTodo.run(result.lastInsertRowid, defaultTodo)
-
+   await prisma.todo.create({
+    data:{
+      task:defaultTodo,
+      userId: user.id
+    }
+   })
     // create a token 
-    const token = jwt.sign({id: result.lastInsertRowid}, process.env.JWT_SECRET, {expiresIn: '24h'})
+    const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '24h'})
     res.json({token})
   }
   catch(err){
@@ -33,12 +41,15 @@ router.post('/register', (req, res)=>{
 })
 
 
-router.post('/login',(req, res)=>{
+router.post('/login',async (req, res)=>{
   const {username, password} = req.body
 
   try{
-    const getUser = db.prepare(`SELECT * FROM users WHERE username = ?`)
-    const user = getUser.get(username)
+    const user = await prisma.user.findUnique({
+      where:{
+        username:username
+      }
+    })
 
     if(!user){
       return res.status(404).send({message: "User not found"})
